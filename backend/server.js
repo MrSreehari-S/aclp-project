64f1083f2f9ac6014fc1d5c7cd71cd72e2706011
MrSreehari-S <sreehari200411@gmail.com>
2026-05-06 20:39:32 +0530

    import error fix

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+
 import connectDB from "./config/db.js";
+
+// Route Imports
+
 import authRoutes from "./routes/authRoutes.js";
 import problemRoutes from "./routes/problemRoutes.js";
 import judgeRoutes from "./routes/judgeRoutes.js";
@@ -10,16 +14,15 @@ import userRoutes from "./routes/userRoutes.js";
 import leaderboardRoutes from "./routes/leaderboardRoutes.js";
 import matchHistoryRoutes from "./routes/matchHistoryRoutes.js";
 
-
-
 connectDB();
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api", require("./routes"));
 app.use("/api/user", userRoutes);
 app.use("/api/judge", judgeRoutes);
 app.use("/api/problems", problemRoutes);
@@ -27,13 +30,15 @@ app.use("/api/leaderboard", leaderboardRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/match", matchRoutes);
 app.use("/api/match", matchHistoryRoutes);
+
+// Root Route
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
-
-
-
-
+// Server Start
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
